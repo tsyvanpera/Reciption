aca5388f68c7b6dc9a0c94b09f78e039d4d1261c
Tommi Syvänperä <tommi.syvanpera@gmail.com>
2017-01-17 07:21:17 +0200

    dom to json - first iteration

--- src/main/java/com/reciption/ReciptionApplication.java
+++ src/main/java/com/reciption/ReciptionApplication.java
@@ -1,19 +1,58 @@
 package com.reciption;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sun.mail.handlers.text_html;
+
 @SpringBootApplication
 public class ReciptionApplication {
-
+	private static int elementcount = 0;
+	
 	public static void main(String[] args) throws IOException {
+		StringBuilder builder = new StringBuilder();
+		
+		Pattern pattern = Pattern.compile("Valmistusaineet");
 		SpringApplication.run(ReciptionApplication.class, args);
-		Document doc = Jsoup.connect("http://www.kodinkuvalehti.fi/artikkeli/ruoka/reseptit/moskovanpata").get();
+		
+		Document doc = Jsoup.connect("http://nimisto.birdlife.fi:3000/haku?hakusanat=parus&nimikentta=kaikki&matching=osa").get();
 
-		System.out.println(doc);
+		Elements elements = doc.getElementsByAttributeValue("class", "nimet");
+		
+		for (Element e: elements) {
+			System.out.println(e);
+			System.out.println(jsonifyElement(e, builder));
+		}
+	}
+	
+	public static StringBuilder jsonifyElement(Node node, StringBuilder builder) {
+		builder.append("{");
+		if (node instanceof TextNode) {
+			TextNode text = (TextNode) node;
+			builder.append("\"text_" + (elementcount++) + "\": \"");
+			builder.append(text.text());
+			builder.append("\",");
+		}
+		if (node.childNodes().size() > 0) {
+			if (node instanceof Element) {
+				Element element = (Element) node;
+				builder.append("\"" + element.tagName() + "_" + (elementcount++) + "\": [");
+				for (Node e: node.childNodes()) {
+					jsonifyElement(e, builder);
+				}
+				builder.append("],");
+			}
+		}
+		builder.append("},");
+		return builder;
 	}
 }
